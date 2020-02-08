6ef2bf52bca382980567a22eca31827b2b364110
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-08 04:45:22 -0500

    Header.js

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import "./Header.css"
+import "./Header.css";
 
 function Header(){
     return (
         <div className="header">
-            <h1>Clicky Game</h1>
+            <h1>Pick your Hero</h1>
             <h2>Click on a superhero to absorb their power. If you click on them more than once then you will die.</h2>
         </div>
     );
