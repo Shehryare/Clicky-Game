7222c42afa2f520eb2f49f25dcaa49d3cde4882a
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-08 04:03:12 -0500

    Updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Container from './components/Container/Container';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 // import logo from './logo.svg';
 
-import React, { Component } from 'react';
-
-class App extends Component {
-  
-
-  
-  
-  
-  
-  
-  
-  
-  render() {
-    return (
-      <div>
-        <Container>
-          <Footer/>
-        </Container>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Header/>
+        <Footer/>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
