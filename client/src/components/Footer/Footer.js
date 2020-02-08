4642001445babc42e281f435b466d4e2cfa2d3c5
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-08 02:32:36 -0500

    added footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'; //importing react 
+import './Footer.css';  //importing css file
+
+//footer function 
+function Footer(props){
+    return(
+        <div className="footer">
+            <p className="center">Github:@Shehryare</p>
+        </div>
+    );
+}
+
+//exporting footer
+export default Footer;
