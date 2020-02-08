199cc67f23e9280ccd56d535e73b1dcbd6e8f7f6
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-08 05:07:04 -0500

    Images component: styling & js updated

---
+++ client/src/components/Images/Images.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Images.css";
+
+
+function Images(props) {
+    return (
+        <div className="card">
+            <img
+                onClick={() => props.handleClicked(props.id)}
+                className="card img"
+                alt={props.name}
+                src={props.image}
+
+
+            />
+        </div>
+    );
+
+}
+
+export default Images;
