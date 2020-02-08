168f37596f138205a8dc70f93fb8d1f91ddd110d
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-08 05:27:24 -0500

    Header component update

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,8 @@ function Header(){
     return (
         <div className="header">
             <h1>Pick your Hero</h1>
-            <h2>Click on a superhero to absorb their power. If you click on them more than once then you will die.</h2>
+            <h2>Click on a superhero to absorb their power.</h2>
+            <h2 id="yellow-warning"> If you click on them more than once then you will die.</h2>
         </div>
     );
 }
