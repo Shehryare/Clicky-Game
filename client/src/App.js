42988a5f5873bc2de92c2113f435370bc2efbcce
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-08 05:28:56 -0500

    updated App js

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import Container from './components/Container/Container';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Images from './components/Images/Images';
+import Navbar from './components/Navbar/Navbar';
 // import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Container>
         <Header/>
         <Footer/>
