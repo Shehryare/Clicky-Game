0aa6e4e44bc2e19aa3bb6c8148de6b8e7d6ba3d2
Shehryar Ehtesham <se318@rutgers.edu>
2020-02-08 04:01:16 -0500

    Header component created

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css"
+
+function Header(){
+    return (
+        <div className="header">
+            <h1>Clicky Game</h1>
+            <h2>Click on a superhero to absorb their power. If you click on them more than once then you will die.</h2>
+        </div>
+    );
+}
+
+export default Header;
